eefed16a9d0a1bd8f234bdb69f02c68ca9d25673
Neel Redkar <neel.redkar@gmail.com>
2021-04-28 00:29:38 -0700

    add collaboration edits

--- pages/_app.js
+++ pages/_app.js
@@ -20,6 +20,35 @@ function App({ Component, pageProps }) {
           <Head>
             <title>TARDIS</title>
             <link rel="icon" href="/tardis.png" />
+            <meta name="title" content="TARDIS" />
+            <meta
+              name="description"
+              content="Take back control of your life with TARDIS, a headset that uses neurotechnology and AI to keep you focused."
+            />
+
+            <meta property="og:type" content="website" />
+            <meta property="og:url" content="https://tardisapp.us" />
+            <meta property="og:title" content="TARDIS" />
+            <meta
+              property="og:description"
+              content="Take back control of your life with TARDIS, a headset that uses neurotechnology and AI to keep you focused."
+            />
+            <meta
+              property="og:image"
+              content="https://tardisapp.usassets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
+            />
+
+            <meta property="twitter:card" content="summary_large_image" />
+            <meta property="twitter:url" content="https://tardisapp.us" />
+            <meta property="twitter:title" content="TARDIS" />
+            <meta
+              property="twitter:description"
+              content="Take back control of your life with TARDIS, a headset that uses neurotechnology and AI to keep you focused."
+            />
+            <meta
+              property="twitter:image"
+              content="https://tardisapp.usassets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
+            />
           </Head>
           <Nav />
           <Component {...pageProps} />
@@ -49,6 +78,59 @@ function App({ Component, pageProps }) {
               />
             </ButtonGroup>
           </Flex>
+          <style jsx global>{`
+            @import url("https://fonts.googleapis.com/css2?family=Mulish&display=swap");
+            @import url("https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap");
+            body {
+              font-family: "Mulish", "Avenir", -system-ui;
+            }
+            .main-title {
+              font-family: "Bebas Neue", "Mulish", "Avenir", -system-ui;
+            }
+            .button {
+              padding: 10px 15px;
+              position: relative;
+              font-weight: 800;
+              font-size: 2rem;
+              margin-top: 15px;
+              margin-left: auto;
+              margin-right: auto;
+              text-align: center;
+
+              color: #ffffff;
+              box-shadow: 0px 4px 30px rgba(255, 255, 255, 0.76);
+              border-radius: 5px;
+              background: linear-gradient(
+                90deg,
+                rgba(16, 226, 255, 0.8) -34.31%,
+                rgba(223, 154, 255, 0.8) 103.95%
+              );
+            }
+            .button:hover {
+              background: linear-gradient(
+                90deg,
+                rgba(16, 198, 255, 0.9) 50%,
+                rgba(218, 139, 255, 0.8) 94.43%
+              );
+              box-shadow: 0px 0px 30px 0px #0eb7ff8a;
+
+              border-radius: 10px;
+            }
+            .section-title {
+              font-family: "Bolden", "Mulish", "Avenir", -system-ui;
+              font-weight: 900;
+            }
+
+            .card-glow > h1 {
+              margin-top: 10px;
+            }
+            .card-glow {
+              box-shadow: 0px 0px 20px 0px #10e2ff33;
+            }
+            .team:hover {
+              box-shadow: 0px 0px 30px 0px #10e2ff54;
+            }
+          `}</style>
         </Box>
       </ChakraProvider>
     </RecoilRoot>
--- pages/index.js
+++ pages/index.js
@@ -10,8 +10,20 @@ import {
   List,
   ListItem,
   ListIcon,
+  Button,
 } from "@chakra-ui/react";
 import { FaExclamationTriangle } from "react-icons/fa";
+
+let Highlight = (props) => (
+  <Text
+    as="span"
+    px="5px"
+    py="0"
+    bg="yellow.200"
+    borderRadius="md"
+    {...props}
+  />
+);
 
 const NumberIcon = (props) => (
   <Box
@@ -35,8 +47,10 @@ const TeamMember = ({ name, title, src, ...props }) => (
     boxShadow="md"
     p="15px"
     borderRadius="lg"
+    mx={["auto", 0, 0]}
     mr="auto"
     my="10px"
+    width="75%"
     {...props}
   >
     <Avatar src={src} />
@@ -51,27 +65,35 @@ export default function Main() {
   return (
     <Box display="flex" flexDir="column">
       <Box mx="10px" mt="30px">
+        <Image
+          sx={{
+            height: "95px",
+            mx: "auto",
+          }}
+          src="/tardis.png"
+        />
         <Flex>
-          <Image
-            sx={{
-              height: "95px",
-              ml: "auto",
-              my: "auto",
-              pr: "20px",
-            }}
-            src="/tardis.png"
-          />
           <Heading
             my="auto"
-            mr="auto"
+            mx="auto"
             fontSize={[45, 75, 95]}
-            fontWeight="bold"
-            color="cyan.500"
+            fontWeight="900"
+            color="#201DCF"
+            className="main-title"
           >
             TARDIS
           </Heading>
         </Flex>
-        <Text textAlign="center" color="gray.300">
+        <Text
+          className="long-title"
+          sx={{
+            fontSize: ["lg", "2xl", "3xl"],
+            py: "10px",
+            fontWeight: "bold",
+          }}
+          textAlign="center"
+          color="gray.300"
+        >
           {"Transcranial Applicable Repeating Deep Interchanging Stimulation"
             .split(" ")
             .map((v) => {
@@ -92,18 +114,31 @@ export default function Main() {
           </Text>{" "}
           of your life.
         </Text>
-        <Text mx="auto" width={["91vw", "62vw", "36vw"]}>
-          TARDIS is a response to the{" "}
-          <strong>widespread pandemic of demotivation</strong> and disinterest
-          of today. By enhancing every different aspect of the day to be
-          higher-quality, we can{" "}
-          <strong>
-            impact the lives of people of all ages and walks of life.
-          </strong>
+        <Text
+          mx="auto"
+          fontSize={[16, 16, 24]}
+          width={["95vw", "65vw", "40vw"]}
+        >
+          TARDIS is our response to the{" "}
+          <strong>widespread pandemic of mental health </strong>
+          of today. By enhancing every aspect of your day to be higher-quality,
+          we can{" "}
+          <strong>impact the lives of people from all walks of life.</strong>
         </Text>
+        <Flex>
+          <Button
+            href="#"
+            as="a"
+            target="_blank"
+            fontWeight="800"
+            className="button"
+          >
+            Our Pitch Deck
+          </Button>
+        </Flex>
       </Box>
       <Divider my="30px" width="100%" mx="auto" />
-      <Box mx={["10px", "30px"]}>
+      <Box mx={["10px", "10vw", "20vw"]}>
         <Flex
           flexDir={["column", "row", "row"]}
           boxShadow="lg"
@@ -112,7 +147,7 @@ export default function Main() {
           my="10px"
         >
           <Box my="auto">
-            <Heading fontSize={[25, 25, 35]}>
+            <Heading mb="10px" fontSize={[25, 25, 35]}>
               How does this{" "}
               <Text
                 sx={{
@@ -127,13 +162,14 @@ export default function Main() {
               ? 👷
             </Heading>
             <Text>
-              We use neurotechnology inside a headset along with AI to identify
-              what state of mind you are in. This could be anything from stress
-              to anxiety. Then, using a tDCS, we can gently modulate the user’s
-              brain waves to be closer to the user’s goal, be it to focus or
-              sleep or stay calm. All of this is saved on the cloud for easy
-              access and provides statistics to help you throughout the day.
-              Having doubts on the science? Check out our{" "}
+              We use state-of-the-art <Highlight>neurotechnology</Highlight>{" "}
+              inside a headset along with <Highlight>AI</Highlight> to identify
+              your state of mind. This could be anything from stress to anxiety.
+              Using a tDCS, TARDIS will <Highlight>gently modulate</Highlight>{" "}
+              your brain waves , be it to focus, sleep, or stay calm. All of the
+              data is saved in the cloud for easy access and provides statistics
+              to help you throughout your day. Having doubts on the science?
+              Check out our{" "}
               <Text
                 sx={{
                   color: "blue.400",
@@ -147,7 +183,7 @@ export default function Main() {
                 target="_blank"
                 as="a"
               >
-                whitepaper supported with multiple research studies
+                whitepaper supported by multiple research studies
               </Text>
               .
             </Text>
@@ -173,7 +209,7 @@ export default function Main() {
               src="/dots.svg"
               sx={{
                 gridColumn: 1,
-                pt: ["1%", "10%"],
+                pt: ["1%", "1%"],
                 pl: "20%",
                 gridRow: 1,
               }}
@@ -189,7 +225,7 @@ export default function Main() {
             m="10px"
           >
             <Box my="auto">
-              <Heading fontSize={[25, 25, 35]}>
+              <Heading fontSize={[25, 25, 35]} mb="10px">
                 Great. How much does this{" "}
                 <Text
                   sx={{
@@ -204,11 +240,11 @@ export default function Main() {
                 ? 💰
               </Heading>
               <Text>
-                We plan on selling TARDIS at{" "}
-                <strong>a low cost subscription</strong>! This way the barrier
-                to access is much lower, allowing for more people. We'll be
-                selling it through mental health clientelle, Amazon, and this
-                website!
+                We plan on providing TARDIS with{" "}
+                <strong>a low cost subscription option</strong>! This way, the
+                barrier to access is much lower, allowing for more people to
+                benefit. We'll sell TARDIS through businesses with
+                mental-health-focused clientele, Amazon, and this website!
               </Text>
             </Box>
           </Flex>
@@ -220,7 +256,7 @@ export default function Main() {
             m="10px"
           >
             <Box my="auto">
-              <Heading fontSize={[25, 25, 35]}>
+              <Heading fontSize={[25, 25, 35]} mb="10px">
                 Hmm. Is this brain stuff{" "}
                 <Text
                   sx={{
@@ -235,16 +271,22 @@ export default function Main() {
                 ? 🦺
               </Heading>
               <Text>
-                We use <strong>100% noninvasive technology</strong> that gives
-                only positive long-term effects, and easy to remove. The entire
-                thing is a small headset you can remove easily, and just
-                attaches to the outside of your head.
+                We use <strong>100% noninvasive technology</strong> that leaves
+                only positive long-term effects. The entire device is a portable
+                headset you can remove easily. TARDIS just attaches to the
+                outside of your head, like a hat. That's right, no wires in your
+                brain like in the movies!
               </Text>
             </Box>
           </Flex>
         </Flex>
-        <Box boxShadow="lg" borderRadius="lg" p="20px" my="10px">
-          <Heading fontSize={[25, 25, 35]}>
+        <Box p="20px" my="10px">
+          <Heading
+            fontSize={[25, 25, 35]}
+            fontWeight="800"
+            padding="10px"
+            className="section-title"
+          >
             How are we better than the{" "}
             <Text
               sx={{
@@ -259,37 +301,79 @@ export default function Main() {
             ?
           </Heading>
           <Text>
-            All alternatives are a whack-a-mole game of having to choose between
+            Our competitors are a whack-a-mole game of having to choose between
             something <strong>inneffective</strong>, <strong>invasive</strong>,
             and <strong>expensive</strong>
           </Text>
           <Text fontWeight="bold">TARDIS does much better by being</Text>
-          <Flex flexDir={["column", "row", "row"]} flexWrap="wrap">
-            <Box boxShadow="lg" borderRadius="lg" p="20px" m="10px" width="30%">
-              <Heading fontSize={[25, 25, 35]}>🦽 Accessible</Heading>
-              <Text>TARDIS is low cost and easy to use and customize.</Text>
-            </Box>
-            <Box boxShadow="lg" borderRadius="lg" p="10px" m="10px" width="30%">
-              <Heading fontSize={[25, 25, 35]}>👆 Interactive</Heading>
-              <Text>
-                The app allows personalized feedback and training, giving the
-                user the choice to choose their goal and stick with it.
-              </Text>
-            </Box>
-            <Box boxShadow="lg" borderRadius="lg" p="20px" m="10px" width="30%">
-              <Heading fontSize={[25, 25, 35]}>💥 Effective</Heading>
-              <Text>
-                We don’t sacrifice quality – our initial prototype tests show it
-                is effective and there is much research supporting our findings.
-              </Text>
-            </Box>
-            <Box boxShadow="lg" borderRadius="lg" p="20px" m="10px" width="30%">
-              <Heading fontSize={[25, 25, 35]}>😇 Ethical</Heading>
-              <Text>
-                We use 100% noninvasive technology that gives only positive
-                long-term effects, and easy to remove.
-              </Text>
-            </Box>
+          <Flex flexDir="column">
+            <Flex flexDir={["column", "row", "row"]} mx="auto">
+              <Box
+                boxShadow="lg"
+                borderRadius="lg"
+                p="20px"
+                m="10px"
+                flex="1"
+                className="card-glow"
+              >
+                <Heading fontSize={[25, 25, 35]} fontWeight="800">
+                  🦽 Accessible
+                </Heading>
+                <Text>TARDIS is low cost, easy to use, and customizable.</Text>
+              </Box>
+              <Box
+                boxShadow="lg"
+                borderRadius="lg"
+                p="10px"
+                m="10px"
+                flex="1"
+                className="card-glow"
+              >
+                <Heading fontSize={[25, 25, 35]} fontWeight="800">
+                  👆 Interactive
+                </Heading>
+                <Text>
+                  The app allows personalized feedback and training, giving the
+                  user the choice to choose their goal and stick with it.
+                </Text>
+              </Box>
+            </Flex>
+            <Flex flexDir={["column", "row", "row"]} mx="auto">
+              <Box
+                boxShadow="lg"
+                borderRadius="lg"
+                p="20px"
+                m="10px"
+                flex="1"
+                className="card-glow"
+              >
+                <Heading fontSize={[25, 25, 35]} fontWeight="800">
+                  💥 Effective
+                </Heading>
+                <Text>
+                  We don’t sacrifice quality – our initial prototype tests show
+                  TARDIS is effective and there is abundant research supporting
+                  our findings.
+                </Text>
+              </Box>
+              <Box
+                boxShadow="lg"
+                borderRadius="lg"
+                p="20px"
+                m="10px"
+                flex="1"
+                className="card-glow"
+              >
+                <Heading fontSize={[25, 25, 35]} fontWeight="800">
+                  😇 Ethical
+                </Heading>
+                <Text>
+                  We use noninvasive, easy-to-use technology. No wires, no
+                  needles jabbing into your head.Say goodbye to "No pain, no
+                  gain" because TARDIS is completely painless!
+                </Text>
+              </Box>
+            </Flex>
           </Flex>
         </Box>
       </Box>
@@ -300,8 +384,8 @@ export default function Main() {
             The Team
           </Heading>
           <Text mx="auto" width="75%">
-            We can be trusted to take this to completion with our diverse team
-            of programmers and scientists!
+            Meet the team behind TARDIS! Our expertise in hardware, software,
+            and neuroscience converge perfectly.
           </Text>
         </Flex>
         <Flex flex="1" flexDir="column" mx="auto">
@@ -309,21 +393,37 @@ export default function Main() {
             name="Claire Wang"
             title="Neuroscience Researcher at UCLA and Hardware Designer"
             src="/claire.jpeg"
+            as="a"
+            href="https://bookworm.design"
+            className="card-glow"
+            className="team"
           />
           <TeamMember
             name="Charles Duong"
             title="Backend Programmer and Computer Science Researcher"
             src="/charles.jpg"
+            as="a"
+            href="https://linkedin.com/in/charlie-duong"
+            className="card-glow"
+            className="team"
           />
           <TeamMember
             name="Andrew Gao"
-            title="Computational Neuroscience Researcher and Scientist"
+            title="Published Computational Neuroscience Researcher and UCSD Intern"
             src="/andrew.png"
+            as="a"
+            href="https://andrewgao.dev"
+            className="card-glow"
+            className="team"
           />
           <TeamMember
             name="Neel Redkar"
             title="AI Development/Researcher and Full Stack Developer"
             src="/neel.png"
+            as="a"
+            href="https://neelr.dev"
+            className="card-glow"
+            className="team"
           />
         </Flex>
       </Flex>
